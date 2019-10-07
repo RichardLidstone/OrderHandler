464c48e3d36f3a6b0d7ac6c305b91bdfc68fc1e3
richard <lidstone@gmail.com>
2019-10-07 16:21:42 +1100

    Fleshed out the Payment processing and tests

--- OrderHandler/OrderHandler/PaymentProcessor.cs
+++ OrderHandler/OrderHandler/PaymentProcessor.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OrderHandler
 {
     public class PaymentProcessor : Common
     {
         private TextWriter writer;
+        private int delay;
 
-        public PaymentProcessor(TextWriter writer)
+        public PaymentProcessor(TextWriter writer, int delay = 2000)
         {
             this.writer = writer;
+            this.delay = delay;
         }
 
-        public void process(Order order)
+
+        public async Task process(Order order)
         {
-            throw new NotImplementedException();
+            if (order.paymentProcessed)                                                                             // if the order payment has already been processed ...
+                throw new ApplicationException("Order payment already processed");                                      // throw an exception - this should never happen
+
+            writer.WriteLine($"Payment Processor#{id}: {timestamp}: Order #{order.id}. Processing Payment.");       // build the start message
+
+            order.paymentProcessed = await paymentIO(order);                                                        // process the order payment - in a real system this is probably going to be have an async IO component ao we use await
+
+            string msg = order.paymentProcessed ? "Payment Processed" : "Payment Failed";                           // determine the message detail on whether the payment was successful or not
+            writer.WriteLine($"Payment Processor#{id}: {timestamp}: Order #{order.id}. {msg}.");                    // build the completion message
+        }
+
+
+        private async Task<bool> paymentIO(Order order)
+        {
+            await Task.Delay(delay);
+            return order.id % 5 != 0;
         }
     }
 }
--- OrderHandler/OrderHandlerTests/PaymentProcessorTests.cs
+++ OrderHandler/OrderHandlerTests/PaymentProcessorTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Concurrent;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace OrderHandlerTests
 {
@@ -12,57 +13,43 @@ namespace OrderHandlerTests
     public class PaymentProcessorTests : TestBase
     {
         [TestMethod]
-        public void processPayment()
+        public async Task processPayment()
         {
             Order order = new Order();
 
-            PaymentProcessor payment = new PaymentProcessor(new StringWriter());
-            payment.process(order);
+            PaymentProcessor payment = new PaymentProcessor(new StringWriter(), 10);
+            await payment.process(order);
 
             Assert.IsTrue(order.paymentProcessed);
         }
 
 
-
-        /*
         [TestMethod]
-        public void DeliveryProcessorOutput()
+        public async Task CantReprocessOrders()
+        {
+            Order order = new Order();
+
+            PaymentProcessor payment = new PaymentProcessor(new StringWriter(), 10);
+            await payment.process(order);
+
+            payment = new PaymentProcessor(new StringWriter());
+
+            await Assert.ThrowsExceptionAsync<ApplicationException>(async () => await payment.process(order));
+        }
+
+        [TestMethod]
+        public async Task DeliveryProcessorOutput()
         {
             StringWriter writer = new StringWriter();
             Order order = new Order();
 
-            DeliveryProcessor deliveryProcessor = new DeliveryProcessor(writer);
-            deliveryProcessor.handleOrder(order);
+            PaymentProcessor payment = new PaymentProcessor(writer, 10);
+            await payment.process(order);
 
-            Regex pattern = new Regex(@"Delivery Processor#\d+: \d{2}:\d{2}\.\d{2} [ap]m: Order #\d+\. Ready for Delivery\.");
+            Regex pattern = new Regex(@"Payment Processor#\d+: \d{2}:\d{2}\.\d{2} [ap]m: Order #\d+\. Processing Payment\.\r\nPayment Processor#\d+: \d{2}:\d{2}\.\d{2} [ap]m: Order #\d+\. Payment Processed\.");
+
             Assert.IsTrue(pattern.IsMatch(writer.ToString()), "got: " + writer.ToString());
         }
 
-
-        [TestMethod]
-        public void DeliveryProcessorDoesDelivery()
-        {
-            Order order = new Order();
-
-            DeliveryProcessor deliveryProcessor = new DeliveryProcessor(new StringWriter());
-            deliveryProcessor.handleOrder(order);
-
-            Assert.IsTrue(order.delivered);
-        }
-
-
-        [TestMethod]
-        public void CantReprocessOrders()
-        {
-            Order order = new Order();
-
-            DeliveryProcessor deliveryProcessor = new DeliveryProcessor(new StringWriter());
-            deliveryProcessor.handleOrder(order);
-
-            deliveryProcessor = new DeliveryProcessor(new StringWriter());
-
-            Assert.ThrowsException<ApplicationException>(() => deliveryProcessor.handleOrder(order));
-        }
-        //*/
     }
 }
